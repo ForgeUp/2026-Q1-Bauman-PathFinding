6f4160da3b00b1ae15523892faa68349e3086fc6
ForgeUp <sh.383.fank@mail.ru>
2026-01-25 20:17:10 +0300

    Мега: Добавлена дискретизация по сетке и поддержка хэширования

--- types/Point.hpp
+++ types/Point.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <ostream>
+#include <iostream>
 
 #include "math/eq.hpp"
+#include "math/trunc.hpp"
 
 
 struct Point {
     double x{0};
     double y{0};
 
-    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
-        os << p.x << ' ' << p.y << '\n';
-        return os;
-    }
+    Point() = default;
+    Point(double x_, double y_) : x{math::trunc(x_)}, y{math::trunc(y_)} {}
 
     friend bool operator==(const Point& a, const Point& b) {
         return math::eq(a.x, b.x) && math::eq(a.y, b.y);
@@ -20,6 +20,27 @@ struct Point {
 
     friend bool operator<(const Point& a, const Point& b) {
         if (!math::eq(a.x, b.x)) return a.x < b.x;
-        return a.y < b.y;
+        if (!math::eq(a.y, b.y)) return a.y < b.y;
+        return false;
+    }
+
+public:
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        os << p.x << ' ' << p.y << '\n';
+        return os;
+    }
+
+};
+
+namespace std {
+
+template<>
+struct hash<Point> {
+    std::size_t operator()(const Point& p) const noexcept {
+        std::size_t hx = std::hash<double>{}(p.x);
+        std::size_t hy = std::hash<double>{}(p.y);
+        return hx ^ (hy << 1);
     }
 };
+
+}
