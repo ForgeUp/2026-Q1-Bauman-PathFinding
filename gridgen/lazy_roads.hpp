b52b675b9969e70ad0f23346bac3d88479c63e1b
ForgeUp <sh.383.fank@mail.ru>
2026-01-25 20:22:41 +0300

    Мини: Вынесено в отдельный файл

--- gridgen/lazy_roads.hpp
+++ gridgen/lazy_roads.hpp
@@ -7,12 +7,14 @@
 #include "types/Task.hpp"
 #include "types/SolverSettings.hpp"
 
+
+namespace gridgen {
+
 using square_idx = std::pair<int64_t,int64_t>;
-
-
+    
 // Генерация дорог БЕЗ проверки пересечения с препятствиями.
-std::vector<Segment> generate_lazy_roads(const Task& task, std::map<square_idx,Point> squares, const SolverSettings& stgs) {
-    std::vector<Segment> roads;
+Graph lazy_roads(const Task& task, std::map<square_idx,Point> squares, const SolverSettings& stgs) {
+    Graph graph;
 
     // Перебираем ячейки с левой нижней и пытаемся соединить точки в соседних ячейках.
     for (int64_t i = 0; i < stgs.squares_per_side; ++i) { // Цикл вдоль оси Y.
@@ -24,16 +26,18 @@ std::vector<Segment> generate_lazy_roads(const Task& task, std::map<square_idx,P
 
             auto& curr_point = squares[curr_square_idx];
 
-            // Проверяем ячейки справа, справа-сверху, сверху на доступность.
-            for (auto near_square_idx : std::vector<square_idx>{{i,j+1}, {i+1,j+1}, {i+1,j}}) {
+            // Проверяем ячейки справа, справа-сверху, сверху, слева-сверху на доступность.
+            for (auto near_square_idx : std::vector<square_idx>{{i,j+1}, {i+1,j+1}, {i+1,j}, {i+1,j-1}}) {
                 // Пропускаем недоступные ячейки.
                 if (!squares.contains(near_square_idx)) continue;
 
                 auto& near_point = squares[near_square_idx];
-                roads.emplace_back(curr_point, near_point);
+                graph.add(curr_point, near_point);
             }
         }
     }
 
-    return roads;
+    return graph;
+}
+
 }
