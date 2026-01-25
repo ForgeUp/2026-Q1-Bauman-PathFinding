59da166d21b4ec11c8d85c50b2f6c2ab70cbe8ca
ForgeUp <sh.383.fank@mail.ru>
2026-01-25 20:30:18 +0300

    Мини: Актуализированы имена функций и структуры данных

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include <format>
 #include <string>
 
-#include "types/Task.hpp"
-
-#include "generate_task.hpp"
+#include "taskgen/task.hpp"
 #include "solve.hpp"
 #include "to_file.hpp"
 
@@ -22,7 +20,7 @@ int main() {
         .polygon_max_radius = 6,
         .border_margin = 5,
     };
-    auto task = generate_task(cfg);
+    auto task = taskgen::task(cfg);
     
     SolverSettings stgs = {
         .squares_per_side = 10,
@@ -30,12 +28,12 @@ int main() {
     };
     auto sln = solve(task, stgs);
 
-    to_file("data/area.gp", std::format("x_min={}\nx_max={}\ny_min={}\ny_max={}\n", task.area.x_min, task.area.x_max, task.area.y_min, task.area.y_max));
+    to_file("data/area.gp", std::format("x_min={}\nx_max={}\ny_min={}\ny_max={}\nx_start={}\ny_start={}\nx_end={}\ny_end={}", task.area.x_min, task.area.x_max, task.area.y_min, task.area.y_max, task.start.x, task.start.y, task.end.x, task.end.y));
     to_file("data/polygons.txt", task.area.rocks);
     to_file("data/grid.txt", sln.grid);
-    to_file("data/examined_roads.txt", sln.examined_roads);
+    to_file("data/examined_roads.txt", sln.examined);
     to_file("data/path.txt", sln.path);
-    to_file("data/invalid_roads.txt", sln.invalid_roads);
+    to_file("data/invalid_roads.txt", sln.invalid);
 
     return 0;
 }
