202dd8716a69ab58376dc3d39a080fb02dd5d95d
ForgeUp <sh.383.fank@mail.ru>
2026-01-26 15:14:36 +0300

    Мини: Добавлена

---
+++ geometry/dist.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cmath>
+
+#include "types/Point.hpp"
+
+
+namespace geometry {
+
+double dist(const Point& a, const Point& b) {
+    return std::sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
+
+}
