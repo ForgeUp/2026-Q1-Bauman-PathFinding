de554c15f9e6777461d9a3572f657a01e3392054
ForgeUp <sh.383.fank@mail.ru>
2026-02-04 21:56:55 +0300

    Мини: Добавлена вариация векторного произведения

--- geometry/cross.hpp
+++ geometry/cross.hpp
@@ -5,8 +5,14 @@
 
 namespace geometry {
 
+// Векторное произведение смещённых векторов p0p1 и q0q1.
+double cross(const Point& p0, const Point& p1, const Point& q0, const Point& q1) {
+    return (p1.x - p0.x) * (q1.y - q0.y) - (p1.y - p0.y) * (q1.x - q0.x);
+}
+
+// Векторное произведение смещённых векторов ab и ac.
 double cross(const Point& a, const Point& b, const Point& c) {
-    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    return cross(a, b, a, c);
 }
 
 }
