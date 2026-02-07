160f4f73d6bdb8cba58384b17f298f1ae0ad9b13
ForgeUp <sh.383.fank@mail.ru>
2026-02-07 14:21:11 +0300

    Мини: Добавлены тесты дерева квадрантов

---
+++ qtree/test.cpp
@@ -0,0 +1,43 @@
+
+#include "qtree.hpp"
+#include "types/Point.hpp"
+
+#include "taskgen/task.hpp"
+#include "draw.hpp"
+
+
+int main() {
+    std::filesystem::path project_root = "D:\\1 Reps\\2026-Q1-Bauman-PathFinding";
+    std::filesystem::current_path(project_root);
+
+    GeneratorConfig cfg = {
+        .x_min = 0,
+        .x_max = 100,
+        .y_min = 0,
+        .y_max = 100,
+
+        .rand_home_points = false,
+        .start = Point(10.0, 10.0),
+        .end   = Point(90.0, 90.0),
+        
+        .polygon_density = 0.05,
+        .polygon_max_vertices = 4,
+        .polygon_min_radius = 1,
+        .polygon_max_radius = 3,
+        .border_margin = 5,
+    };
+    auto task = taskgen::task(cfg);
+
+    Qtree qtree(Point(cfg.x_min,cfg.y_min), Point(cfg.x_max,cfg.y_max));
+
+    // task.area.rocks = std::vector<Rock>{
+    //     Rock{{Point(20, 70), Point(80, 70), Point(50, 20)}}
+    // };
+    for (const auto& r : task.area.rocks) {
+        for (const auto& s : r.edges()) {
+            qtree.add(s);
+        }
+    }
+
+    draw(task, {.qtree = qtree}, "qtree");
+}
