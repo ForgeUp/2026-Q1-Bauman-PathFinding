cc7d8c81a028d60ca0e756275403d5960af42eaf
ForgeUp <sh.383.fank@mail.ru>
2026-02-10 16:15:14 +0300

    Мини: Наведён порядок в тестах дерева квадрантов

--- qtree/test.cpp
+++ qtree/test.cpp
@@ -6,11 +6,40 @@
 #include "draw.hpp"
 
 
-int main() {
-    std::filesystem::path project_root = "D:\\1 Reps\\2026-Q1-Bauman-PathFinding";
-    std::filesystem::current_path(project_root);
+void test1() {
+    Task task = {
+        .area = Area{
+            .x_min = 0,
+            .x_max = 100,
+            .y_min = 0,
+            .y_max = 100,
+        }
+    };
 
-    GeneratorConfig cfg = {
+    Qtree qtree(Point(task.area.x_min,task.area.y_min), Point(task.area.x_max,task.area.y_max));
+    qtree.add(Rock{{Point(20, 70), Point(80, 70), Point(50, 20)}});
+    
+    draw(task, {.qtree = qtree}, "qtree");
+}
+
+void test2() {
+    Task task = {
+        .area = Area{
+            .x_min = 0,
+            .x_max = 100,
+            .y_min = 0,
+            .y_max = 100,
+        }
+    };
+
+    Qtree qtree(Point(task.area.x_min,task.area.y_min), Point(task.area.x_max,task.area.y_max));
+    qtree.add(Rock{{Point(20, 20), Point(80, 80), Point(80, 20)}});
+    
+    draw(task, {.qtree = qtree}, "qtree");
+}
+
+void test3() {
+        GeneratorConfig cfg = {
         .x_min = 0,
         .x_max = 100,
         .y_min = 0,
@@ -29,13 +58,18 @@ int main() {
     auto task = taskgen::task(cfg);
 
     Qtree qtree(Point(cfg.x_min,cfg.y_min), Point(cfg.x_max,cfg.y_max));
-
-    // task.area.rocks = std::vector<Rock>{
-    //     Rock{{Point(20, 70), Point(80, 70), Point(50, 20)}}
-    // };
     for (const auto& r : task.area.rocks) {
         qtree.add(r);
     }
-    
+
     draw(task, {.qtree = qtree}, "qtree");
+}
+
+int main() {
+    std::filesystem::path project_root = "D:\\1 Reps\\2026-Q1-Bauman-PathFinding";
+    std::filesystem::current_path(project_root);
+
+    test1();
+    test2();
+    test3();
 }
