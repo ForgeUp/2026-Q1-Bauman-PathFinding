691cd40f35bff57e05ba291a2553eadda35027ce
ForgeUp <sh.383.fank@mail.ru>
2026-03-09 16:17:37 +0300

    Микро: Добавлен заголовочный файл со всеми решателями

---
+++ solver/Solver/all.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#include "solver/Solver/Article2001.hpp"
+#include "solver/Solver/Bridge.hpp"
+#include "solver/Solver/ClusterOffset.hpp"
+#include "solver/Solver/Gauss.hpp"
+#include "solver/Solver/Naive.hpp"
+#include "solver/Solver/ObstacleOffset.hpp"
+#include "solver/Solver/Qtree.hpp"
