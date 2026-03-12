ce083da1f5c6054f45b56298b90ab255673fb416
ForgeUp <sh.383.fank@mail.ru>
2026-03-12 09:15:32 +0300

    Мини: Баг: Смещение выполняется в пределах разрешённой области

--- gridgen/lazy_offset_grid_sharp.hpp
+++ gridgen/lazy_offset_grid_sharp.hpp
@@ -6,6 +6,7 @@
 #include "types/Area.hpp"
 
 #include "geometry/shift.hpp"
+#include "geometry/clip.hpp"
 #include "geometry/line_intersection.hpp"
 
 
@@ -13,20 +14,27 @@ namespace gridgen {
 
 // Генерация множества точек вокруг препятствий без проверки коллизии.
 Graph lazy_offset_grid_sharp(const Area& area, const Point& min, const Point& max) {
-    Graph points;
+    Graph grid;
 
     double delta = 1; 
+
+    // Функция для корректного смещения ребра графа, чтобы ребро осталось внутри разрешённой области.
+    auto shift = [&min,&max](const Segment& s, double delta) -> Segment {
+        Segment shifted = geometry::shift(s, delta);                     // Смещение отрезка.
+        auto [clipped, is_success] = geometry::clip(shifted, min, max);  // Обрезка отрезка по разрешённой области.
+        return is_success ? clipped : s;
+    };
 
     for (const auto& r : area.rocks) { // Рассмотрение всех препятствий.
         auto& ps = r.points;
 
         // Изначально смещение строится для первого и последнего сегмента многоугольника.
-        Segment first = geometry::shift({ps[0],     ps[1]}, delta);
-        Segment last  = geometry::shift({ps.back(), ps[0]}, delta);
+        Segment first = shift({ps[0],     ps[1]}, delta);
+        Segment last  = shift({ps.back(), ps[0]}, delta);
         auto [first_last_p0, is_intersect] = geometry::line::intersection(last, first);
         if (!is_intersect) {
             first_last_p0 = first.p1;
-            points.add(last.p2, first.p1);
+            grid.add(last.p2, first.p1);
         }
 
         Segment prev = first;
@@ -34,23 +42,23 @@ Graph lazy_offset_grid_sharp(const Area& area, const Point& min, const Point& ma
         
         for (int32_t i = 1; i < ps.size(); ++i) { // Рассмотрение всех рёбер. Начинаем со 2-го ребра.
             Segment s {ps[i], ps[(i+1) % ps.size()]}; // Исходное ребро.
-            Segment curr = geometry::shift(s, delta); // Смещённое ребро.
+            Segment curr = shift(s, delta); // Смещённое ребро.
 
             auto [curr_p0, is_intersect] = geometry::line::intersection(prev, curr);
             if (is_intersect) {
-                points.add(prev_p0, curr_p0);
+                grid.add(prev_p0, curr_p0);
                 prev_p0 = curr_p0;
             } else {
-                points.add(prev_p0, prev.p2);
-                points.add(prev.p2, curr.p1); // Вставляем ребро-перемычку.
+                grid.add(prev_p0, prev.p2);
+                grid.add(prev.p2, curr.p1); // Вставляем ребро-перемычку.
                 prev_p0 = curr.p1;                
             }
             prev = curr;
         }
-        points.add(first_last_p0, prev_p0); // Добавляем последнее ребро.
+        grid.add(first_last_p0, prev_p0); // Добавляем последнее ребро.
     }
 
-    return points;
+    return grid;
 }
 
 }
