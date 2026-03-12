012e1cb33cde397a0bdb7968ac20d17f96e76e43
ForgeUp <sh.383.fank@mail.ru>
2026-03-12 09:12:09 +0300

    Мини: Баг: Коллинеарные отрезки корректно обрабатываются

--- geometry/intersect.hpp
+++ geometry/intersect.hpp
@@ -7,6 +7,7 @@
 #include "is_inside.hpp"
 #include "on_segment.hpp"
 #include "cross.hpp"
+#include "overlap.hpp"
 
 
 namespace geometry {
@@ -17,19 +18,30 @@ bool intersect(const Segment& s1, const Segment& s2) {
     const Point& b = s1.p2;
     const Point& c = s2.p1;
     const Point& d = s2.p2;
-
-    double c1 = geometry::cross(a, b, c);
-    double c2 = geometry::cross(a, b, d);
-    double c3 = geometry::cross(c, d, a);
-    double c4 = geometry::cross(c, d, b);
-
-    if (c1 * c2 < 0 && c3 * c4 < 0) return true;
-
+    
     const double EPS = 1e-9;
-    if (std::fabs(c1) < EPS && geometry::on_segment(a, b, c)) return true;
-    if (std::fabs(c2) < EPS && geometry::on_segment(a, b, d)) return true;
-    if (std::fabs(c3) < EPS && geometry::on_segment(c, d, a)) return true;
-    if (std::fabs(c4) < EPS && geometry::on_segment(c, d, b)) return true;
+
+    auto orient = [EPS](const Point& a, const Point& b, const Point& c)
+    {
+        double v = geometry::cross(a,b,c);
+        if (v >  EPS) return  1;
+        if (v < -EPS) return -1;
+        return 0;
+    };
+
+    int o1 = orient(a,b,c);
+    int o2 = orient(a,b,d);
+    int o3 = orient(c,d,a);
+    int o4 = orient(c,d,b);
+
+    // Обычное пересечение.
+    if (o1 != o2 && o3 != o4) return true;
+
+    // Случаи коллинеарности.
+    if (o1 == 0 && overlap(a.x,b.x,c.x,c.x) && overlap(a.y,b.y,c.y,c.y)) return true;
+    if (o2 == 0 && overlap(a.x,b.x,d.x,d.x) && overlap(a.y,b.y,d.y,d.y)) return true;
+    if (o3 == 0 && overlap(c.x,d.x,a.x,a.x) && overlap(c.y,d.y,a.y,a.y)) return true;
+    if (o4 == 0 && overlap(c.x,d.x,b.x,b.x) && overlap(c.y,d.y,b.y,b.y)) return true;
 
     return false;
 }
