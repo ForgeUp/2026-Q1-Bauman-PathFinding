0554cc4ae650d678ef82594a46a42685408a44b1
ForgeUp <sh.383.fank@mail.ru>
2026-03-17 15:15:55 +0300

    Микро: Добавлено сравнение алгоритмов на загромождённой арене

--- demo/test_classic_and_lazy_PRM.hpp
+++ demo/test_classic_and_lazy_PRM.hpp
@@ -2,6 +2,11 @@
 
 #include "Solver/Solver/Uniform.hpp"
 
+#include "types/GeneratorConfig.hpp"
+#include "types/SolverSettings.hpp"
+
+#include "taskgen/task.hpp"
+
 #include "demo/Data.hpp"
 #include "demo/examples.hpp"
 #include "demo/tester.hpp"
@@ -38,6 +43,35 @@ void test_classic_and_lazy_PRM(VisualizerService& srv_visual) {
         data.stgs.lazy   = true;
         demo::tester<Solver>(data);
     }
+
+    GeneratorConfig cfg = {
+        .x_min = 0,
+        .x_max = 100,
+        .y_min = 0,
+        .y_max = 100,
+
+        .generate_rand_seed = true,
+        .seed = 42,
+
+        .rand_home_points = false,
+        .start = Point(10.0, 10.0),
+        .end   = Point(90.0, 90.0),
+        
+        .polygon_density = 0.05,
+        .polygon_max_vertices = 4,
+        .polygon_min_radius = 1,
+        .polygon_max_radius = 3,
+        .border_margin = 5,
+    };
+    data.task = taskgen::task(cfg);
+
+    data.folder_mark = "classic random";
+    data.stgs.lazy   = false;
+    demo::tester<Solver>(data);
+
+    data.folder_mark = "lazy random";
+    data.stgs.lazy   = true;
+    demo::tester<Solver>(data);
 }
 
 }
