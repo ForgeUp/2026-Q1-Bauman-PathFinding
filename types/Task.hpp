1d4ef36fdfc5d8fbf5ffdfe6f030c79d97e9511b
ForgeUp <sh.383.fank@mail.ru>
2026-04-05 13:54:20 +0300

    Мини: Добавлена функция для создания копии структуры без препятствий

--- types/Task.hpp
+++ types/Task.hpp
@@ -10,3 +10,16 @@ struct Task {
 
     Area area{};
 };
+
+Task no_rocks(const Task& task) {
+    return {
+        .start = task.start,
+        .end   = task.end,
+        .area  = Area{
+            .x_min = task.area.x_min,
+            .x_max = task.area.x_max,
+            .y_min = task.area.y_min,
+            .y_max = task.area.y_max,
+        }
+    };
+}
