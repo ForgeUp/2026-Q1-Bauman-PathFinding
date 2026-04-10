57180d902ef403622a9adea88832e9ddd091d6d1
ForgeUp <sh.383.fank@mail.ru>
2026-04-10 11:35:15 +0300

    Меди: Добавлена функция для вызова gnuplot

---
+++ gnuplot/exec.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <format>
+#include <filesystem>
+
+#include <utils/concat.hpp>
+
+
+namespace gnuplot {
+
+struct Options {
+    std::string script_name;
+    std::string script_dir_path;
+    std::string filename;
+    std::string output_dir_path;
+    std::string data_dir_path;
+};
+
+void exec(const Options& opts) {
+    std::string script_path = opts.script_dir_path + '/' + opts.script_name;
+    if (!std::filesystem::exists(opts.script_dir_path) ||  !std::filesystem::is_directory(opts.script_dir_path)) {
+        std::cerr << "Error: Can't locate directory with gnuplot script: " << opts.script_dir_path << '\n';
+        return;
+    }
+    if (!std::filesystem::exists(script_path)) {
+        std::cerr << "Error: Can't locate gnuplot script: " << script_path << '\n';
+        return;
+    }
+    if (!std::filesystem::exists(opts.output_dir_path) ||  !std::filesystem::is_directory(opts.output_dir_path)) {
+        std::cerr << "Warning: Can't locate output directory for gnuplot: " << opts.output_dir_path << " - directory is created" << '\n';
+        std::filesystem::create_directories(opts.output_dir_path);
+        return;
+    }
+
+    std::string cmd = concat(
+        "gnuplot -e ",
+        "\"",
+        std::format("filename='{}.png'", opts.filename), "; ",
+        std::format("output_dir='{}'", opts.output_dir_path),   "; ",
+        std::format("data_dir='{}'", opts.data_dir_path), 
+        "\" ",
+        script_path, 
+        " > ", "\"", opts.data_dir_path + '/' + "log.txt", "\""
+    );
+    system(cmd.c_str());
+}
+
+}
