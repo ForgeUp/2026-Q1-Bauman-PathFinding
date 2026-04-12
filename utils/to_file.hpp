b3c5ca278b95dca9e432ddcb165cbba17552a394
ForgeUp <sh.383.fank@mail.ru>
2026-04-12 14:40:51 +0300

    Миди: Наведён порядок в шаблонах вывода в файл

--- utils/to_file.hpp
+++ utils/to_file.hpp
@@ -6,29 +6,60 @@
 #include <type_traits>
 
 
+namespace io {
+
+struct Row {
+    std::vector<double> data;
+
+    friend std::ofstream& operator<<(std::ofstream& os, const Row& row) {
+        for (const auto& val : row.data) {
+            os << val << ' ';
+        } 
+        return os;
+    }
+};
+
+struct Options {
+    std::vector<std::string> head;
+};
+
 template <typename T>
-void to_file(std::ofstream& fs, const T& val) {
+void append(std::ofstream& fs, const T& val) {
     fs << val << '\n';
 }
 
 template <typename T1, typename T2>
-void to_file(std::ofstream& fs, const std::pair<T1,T2>& p) {
+void append(std::ofstream& fs, const std::pair<T1,T2>& p) {
     fs << p.first << ' ' << p.second << '\n';
 }
 
-template <typename T>
-concept NotString = !std::is_same_v<std::decay_t<T>, std::string>;
+void append(std::ofstream& fs, const std::string& str) {
+    fs << str << '\n';
+}
 
 template <std::ranges::range Range>
-    requires NotString<Range>
-void to_file(std::ofstream& fs, const Range& r) {
+void append(std::ofstream& fs, const Range& r) {
     for (const auto& obj : r) {
-        to_file(fs, obj);
+        append(fs, obj);
     }
 }
 
+template <typename T>
+void to_file(std::ofstream& fs, const T& val, const io::Options& opts = {}) {
+    if (opts.head.size() > 0) {
+        for (const auto& str : opts.head) {
+            fs << '"' << str << '"' << ' ';
+        }
+        fs << '\n';
+    }
+    append(fs, val);
+}
+
 template <typename T, typename Path>
-void to_file(const Path& filename, const T& val) {
+    requires (!std::is_same_v<std::decay_t<Path>, std::ofstream>)
+void to_file(const Path& filename, const T& val, const io::Options& opts = {}) {
     std::ofstream fs(filename);
-    to_file(fs, val);
+    to_file(fs, val, opts);
+}
+
 }
